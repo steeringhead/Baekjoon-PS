d4ac56e8fac285af352b7a11572ae374cd46b557
정영훈 <131974317+steeringhead@users.noreply.github.com>
2023-05-08 16:49:12 +0900

    [Silver III] Title: 주유소, Time: 56 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+const int maxInput = 100001;
+
+int dist[maxInput];
+int price[maxInput];
+
+
+int main() {
+	int N;
+	cin >> N;
+
+	for (int i = 2; i <= N; i++) {
+		int tmp;
+		cin >> tmp;
+		dist[i] = tmp;
+	}
+
+	for (int i = 1; i <= N; i++) {
+		int tmp;
+		cin >> tmp;
+		price[i] = tmp;
+	}
+
+	int curPrice = price[1];
+	int curIdx = 2;
+	int total = 0;
+
+	while (curIdx <= N) {
+		if (curPrice >= price[curIdx]) {
+			total += curPrice * dist[curIdx];
+			curPrice = price[curIdx];
+			curIdx++;			
+		}
+		else {
+			total += curPrice * dist[curIdx];
+			curIdx++;
+		}
+	}
+	
+	cout << total;
+}

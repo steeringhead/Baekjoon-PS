f21ae1d8d2b523b0067038ba313ca06e0afda1d9
정영훈 <131974317+steeringhead@users.noreply.github.com>
2023-08-04 19:15:42 +0900

    [Gold IV] Title: 여행 가자, Time: 8 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.cc
+++ 백준/Gold/1976. 여행 가자/여행 가자.cc
@@ -1,73 +1,79 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <queue>
 
 using namespace std;
+#define MAX 201
 
-int N, M;
-int destList[1000];
-int adjArr[201][201];
-int myParent[201];
+int parent[MAX];
+int dest[1001];
+int adj[MAX][MAX];
 
-int findNode(int x)
+int find(int x)
 {
-	if (myParent[x] != x)
+	if (x != parent[x])
 	{
-		return findNode(myParent[x]);
+		return parent[x] = find(parent[x]);
 	}
 	else
-		return myParent[x];
+		return x;
 }
 
-void uinonNode(int x, int y)
+void Union(int x, int y)
 {
-	x = findNode(x);
-	y = findNode(y);
+	x = find(x);
+	y = find(y);
+	
+	if (x == y)
+		return;
 
-	if (x < y)
-		myParent[y] = x;
+	if (x > y)
+	{
+		parent[x] = y;
+	}
 	else
-		myParent[x] = y;
-		
+		parent[y] = x;
 }
+
+
 
 int main()
 {
+	int N,M;
 	cin >> N >> M;
-	
+
 	for (int i = 1; i <= N; i++)
 	{
-		myParent[i] = i;
+		parent[i] = i;
 	}
 
 	for (int i = 1; i <= N; i++)
 	{
 		for (int j = 1; j <= N; j++)
 		{
-			cin >> adjArr[i][j];
-			if (adjArr[i][j] == 1)
-				uinonNode(i, j);
+			cin >> adj[i][j];
+			if (adj[i][j] == 1)
+				Union(i, j);
 		}
 	}
 
-	for (int i = 0; i < M; i++)
+	for (int i = 1; i <= M; i++)
 	{
-		cin >> destList[i];
-		
+		cin >> dest[i];
 	}
 
-	int oneValue = findNode(destList[0]);
-
-	for (int i = 1; i < M; i++)
+	int goal = find(dest[1]);
+	
+	for (int i = 2; i <= M; i++)
 	{
-		if (oneValue != findNode(destList[i]))
+		if (find(dest[i]) != goal)
 		{
-			cout << "NO" << endl;
+			cout << "NO";
 			return 0;
 		}
-		
 	}
 
-	cout << "YES" << endl;
+	cout << "YES";
 	return 0;
-	
 }

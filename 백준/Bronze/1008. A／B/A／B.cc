6f4e54ce076e0e66fc283b115c3ebf6917d66102
정영훈 <131974317+steeringhead@users.noreply.github.com>
2023-08-21 19:01:19 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+
+
+int main()
+{
+	double A, B;
+
+	cin >> A >> B;
+
+	cout.precision(9);
+	cout << fixed;
+	cout << A / B;
+}
